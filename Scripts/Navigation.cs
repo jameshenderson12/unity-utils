662a42319195aa8d33fc64828fa90d73dba54d53
James Henderson <64137708+jameshenderson12@users.noreply.github.com>
2021-03-23 14:29:19 +0000

    Add files via upload

---
+++ Scripts/Navigation.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class Navigation : MonoBehaviour
+{
+
+    public void NextScene(string sceneName)
+    {
+      // Load the next scene
+      SceneManager.LoadScene(sceneName);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+/*
+    public void ReturnToHome()
+    {
+      ControlUI.SetActive(false);
+      ControlUI.GetComponent<Renderer>().enabled = false;
+      Destroy(ControlUI);
+      // Stop any current video playing
+      if (!video)
+      {
+          video = FindObjectOfType<VideoPlayer>();
+      }
+      video.Stop();
+      // Load the next scene
+      NextScene("MenuScene");
+      // Disable the control UI
+
+      Debug.Log("Succesfully returning to home");
+    }
+    */
+    
+}
